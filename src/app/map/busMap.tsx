1e8c6a541c60153bae2f48a700a34b1319be5347
Matt Lalonde <matt.ap.lalonde@gmail.com>
2024-11-04 10:24:33 +0000

    controlled camera and line width updates with zoom

--- src/app/map/busMap.tsx
+++ src/app/map/busMap.tsx
@@ -1,8 +1,14 @@
 "use client";
 
-import { APIProvider, Map, Marker } from "@vis.gl/react-google-maps";
+import {
+  APIProvider,
+  Map,
+  MapCameraChangedEvent,
+  MapCameraProps,
+  Marker,
+} from "@vis.gl/react-google-maps";
 import { DeckGLOverlay } from "./deckGLOverlay";
-import { useCallback, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { ColumnLayer, PathLayer } from "@deck.gl/layers";
 import type { PickingInfo } from "@deck.gl/core";
 import {
@@ -19,9 +25,23 @@ export interface BusMapProps {
   lng: number;
 }
 
+const INITIAL_CAMERA = {
+  center: { lat: 51.517867, lng: -0.086384 },
+  zoom: 15,
+};
+
 export function BusMap({ lineData, lat, lng }: BusMapProps) {
   const [mapIsReady, setMapIsReady] = useState(false);
   const handleTilesLoaded = useCallback(() => setMapIsReady(true), []);
+
+  const [cameraProps, setCameraProps] =
+    useState<MapCameraProps>(INITIAL_CAMERA);
+  const handleCameraChange = useCallback(
+    (ev: MapCameraChangedEvent) => setCameraProps(ev.detail),
+    []
+  );
+
+  const [lineWidth, setLineWidth] = useState(10);
 
   const lineLayers = useMemo(() => {
     return [
@@ -30,7 +50,7 @@ export function BusMap({ lineData, lat, lng }: BusMapProps) {
         data: lineData,
         getPath: (d) => d.path,
         getColor: (d) => [d.colour.red, d.colour.green, d.colour.blue],
-        getWidth: 10,
+        getWidth: lineWidth,
         pickable: true,
       }),
       ...lineData.map((d) => {
@@ -46,14 +66,34 @@ export function BusMap({ lineData, lat, lng }: BusMapProps) {
         });
       }),
     ];
-  }, [lineData]);
+  }, [lineData, lineWidth]);
+
+  /* update camera with new lat/lng props */
+  useEffect(() => {
+    setCameraProps((prev) => ({
+      ...prev,
+      center: { lat, lng },
+    }));
+  }, [lat, lng]);
+
+  /* update line width with zoom */
+  useEffect(() => {
+    let newLineWidth = 10;
+    if (cameraProps.zoom < 15 && cameraProps.zoom > 13) {
+      newLineWidth = cameraProps.zoom * 2;
+    } else if (cameraProps.zoom <= 13) {
+      newLineWidth = cameraProps.zoom * 4;
+    }
+
+    setLineWidth(newLineWidth);
+  }, [cameraProps.zoom]);
 
   return (
     <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string}>
       <Map
-        defaultCenter={{ lat, lng }}
-        defaultZoom={15}
+        {...cameraProps}
         onTilesLoaded={handleTilesLoaded}
+        onCameraChanged={handleCameraChange}
       >
         <Marker position={{ lat, lng }} />
         {mapIsReady && (
